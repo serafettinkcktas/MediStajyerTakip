bdc3f89be12cb225458aa5c7727abbd6bbb8401e
serafettinkcktas <kucuktas255@gmail.com>
2026-08-04 10:18:24 +0300

    add: mentor,account,userprofile ctors added

--- Application/UseCases/Admin/AddMentorUseCase.cs
+++ Application/UseCases/Admin/AddMentorUseCase.cs
@@ -1,6 +1,7 @@
 using Application.Common.Helpers;
 using Application.Common.Models;
 using Application.DTOs;
+using Domain.Entity;
 using Domain.Interfaces;
 
 namespace Application.UseCases.Admin;
@@ -17,26 +18,50 @@ public class AddMentorUseCase(IAccountRepository accountRepository, IRoleReposit
     /// <param name="surname"></param>
     /// <param name="email"></param>
     /// <returns>  </returns>
-    public async Task<Result<CreateUserResponseDto>> AddMentorAsync(string name,string surname, string email)
+    public async Task<Result<CreateUserResponseDto>> AddMentorAsync(string name, string surname, string email)
     {
         var exists = await _accountRepository.IsUserExists(email);
         if (exists)
             return Result<CreateUserResponseDto>.Failure(ResultCode.EmailExists, "Bu email kayitli");
-        var roleId = await _roleRepository.GetRoleByNameAsync("Mentor");
-        var accountId = Guid.NewGuid();
+
+        var role = await _roleRepository.GetRoleByNameAsync("Mentor");
+        if (role is null)
+            return Result<CreateUserResponseDto>.Failure(ResultCode.RoleNotFound, "Mentor rolu bulunamadi");
+        
         var password = PasswordHelper.Generate();
-        var passwordHash = PasswordHelper.Hash(password); 
-        var isCreated = await _accountRepository.CreateUser(accountId, name,surname,email,roleId,passwordHash,"Mentor");
+        var passwordHash = PasswordHelper.Hash(password);
+        var accountId = Guid.NewGuid();
+        var profileId = Guid.NewGuid();
+        var mentorId = Guid.NewGuid();
+        Account account = new(
+            accountId,
+            email,
+            passwordHash,
+            role.Id
+        );
+
+        UserProfile userProfile = new(
+            profileId,
+            accountId,
+            name,
+            surname
+        );
+        Mentor mentor = new(
+            mentorId,
+            accountId,
+            profileId
+            );
+        var isCreated = await _accountRepository.CreateUser(account,userProfile,mentor);
+
+        if (!isCreated)
+            return Result<CreateUserResponseDto>.Failure(ResultCode.UnexpectedError, "Hesap olusturulamadi.");
+
         var responseDto = new CreateUserResponseDto
         {
             UserId = accountId,
             GeneratedPassword = password
         };
-        if(isCreated)
-        {
-            return Result<CreateUserResponseDto>.Success(responseDto,"Hesap basariyla olusturuldu.");
-        }
-        return Result<CreateUserResponseDto>.Failure(ResultCode.UnexpectedError, "Hesap olusturulamadi.");
-       
+
+        return Result<CreateUserResponseDto>.Success(responseDto, "Hesap basariyla olusturuldu.");
     }
 }
