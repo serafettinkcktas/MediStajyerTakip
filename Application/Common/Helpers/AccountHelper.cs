ea7f4e6520236d99144eccc7c13bc6c4266c7c21
serafettinkcktas <kucuktas255@gmail.com>
2026-08-05 12:03:25 +0300

    add: account helper class added

--- Application/Common/Helpers/AccountHelper.cs
+++ Application/Common/Helpers/AccountHelper.cs
@@ -6,13 +6,15 @@ public class AccountHelper
 {
     
     //TODO => usecase içindeki account oluşturma kısmını buraya userprofili yeni bir metoda ve mentoru da yeni bir metoda aktar 
-    public async Task<Account> CreateAccount(Guid accountId, string name,string surname,string email,string roleId)
+    public async Task<Account> CreateAccount(Guid id,string email,string passwordHash, Role role)
     {
         Account account = new Account(
-            
+            id,
+            email,
+            passwordHash,
+            role.Id
             );
-        return 
-        );
+        return account;
 
     }
 
